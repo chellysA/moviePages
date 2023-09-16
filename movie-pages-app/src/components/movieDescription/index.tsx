a994f483d275457784eef4aae7d90fb0f7c43f88
Chellys Castillo <chellys.castillo@gmail.com>
2023-09-16 00:23:28 -0400

    fix

--- movie-pages-app/src/components/movieDescription/index.tsx
+++ movie-pages-app/src/components/movieDescription/index.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
 import { IFilmPosterProps } from '../filmPosters';
+import Button from '../button';
+import { BsFillPlayFill } from 'react-icons/bs';
 
 const MovieDescription = ({
   original_title,
@@ -6,9 +9,10 @@ const MovieDescription = ({
   vote_average,
   release_date,
   genre_ids,
+  id,
 }: IFilmPosterProps) => {
   return (
-    <div className="absolute bottom-[70px] px-8">
+    <div className="px-8">
       <h1 className="text-gray-100 ">{original_title}</h1>
       <div className="flex text-gray-100 text-[13px]">
         <p className="pr-2">IMDB: {vote_average}</p>
@@ -16,13 +20,13 @@ const MovieDescription = ({
         <p className="pr-2">Genre: {genre_ids} </p>
       </div>
       <p className="text-gray-50 w-3/4 mb-4">{overview}</p>
-      {/*  carga siempre la misma pelicula  <Link to={`/movie/${id}`} className="no-underline">
+      <Link to={`/movie/${id}`} className="no-underline flex w-max">
         <Button
           icon={<BsFillPlayFill className="h-[30px] w-[30px]" />}
           label="Whatch now"
           border={true}
-  />
-      </Link>*/}
+        />
+      </Link>
     </div>
   );
 };
