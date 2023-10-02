6b9f7650c7c9dd3ba922f2df1acbec4010573ec3
chellysA <chellys.castillo@gmail.com>
2023-10-02 11:11:32 -0400

    fix: changed api call in movies

--- src/hooks/api/useGetMovies.ts
+++ src/hooks/api/useGetMovies.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import env from '../../constants/Enviroments';
-import { addMovies } from '../../redux/moviesSlice';
-import { addActualPage, addTotalPages } from '../../redux/pagerSlice';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import env from "../../constants/Enviroments";
+import { addMovies } from "../../redux/moviesSlice";
+import { addActualPage, addTotalPages } from "../../redux/pagerSlice";
 
 const useGetMovies = (actualPage: string | null) => {
   const dispatch = useDispatch();
@@ -11,17 +11,19 @@ const useGetMovies = (actualPage: string | null) => {
     const {
       data: { results, total_pages, page },
     } = await axios.get(
-      `${env.API_URL}/movie/popular?language=en-US&page=${actualPage}`,
+      `${env.API_URL}/trending/movie/day?language=en-US&page=${actualPage}`,
       {
         params: {
           api_key: env.API_KEY,
         },
       }
     );
+    console.log(total_pages);
     dispatch(addMovies(results));
     dispatch(addTotalPages(total_pages > 500 ? 500 : total_pages));
     dispatch(addActualPage(page));
   };
+
   return { getMovies };
 };
 
