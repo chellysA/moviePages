279f3b3cf0b6865811809cfe8eeea5bdb8c82707
chellysA <chellys.castillo@gmail.com>
2023-10-04 13:52:49 -0400

    responsive design for film posters

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement } from "react";
 
 interface IButtonProps {
   icon?: ReactElement;
@@ -13,7 +13,7 @@ const Button = ({ icon, label, bg, border }: IButtonProps) => {
       <button
         className={`flex ${
           bg && `bg-principal-200`
-        } items-center py-1 text-principal-200 font-[15px] ${
+        } items-center py-1 text-principal-200 text-[74%] ${
           border && `border border-principal-200`
         } pl-3 pr-4 pt-1 rounded-full hover:opacity-50 z-1`}
       >
