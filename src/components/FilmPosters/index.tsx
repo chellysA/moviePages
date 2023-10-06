4be0664b4be285c0a93f4ef591cbc82af8184dc4
chellysA <chellys.castillo@gmail.com>
2023-10-06 12:40:35 -0400

    fixes

--- src/components/FilmPosters/index.tsx
+++ src/components/FilmPosters/index.tsx
@@ -38,13 +38,13 @@ const FilmPosters = ({
     <>
       <div
         id="filmPoster"
-        className="relative w-[200px] h-[300px] mb-[120px] mx-6"
+        className={`relative w-[40%] h-[60%] md:w-[200px] md:h-[300px] mb-[10%] md:mb-[120px] mx-2 md:mx-6`}
       >
         <div id="nohover" className="">
           <img
             src={poster_path}
             alt=""
-            className="rounded-md min-h-[300px] max-h-[300px]"
+            className="rounded-md min-h-[200px] w-full max-h-[300px]"
           ></img>
           {mobile && (
             <Link
@@ -62,11 +62,13 @@ const FilmPosters = ({
             {original_title || name}
           </p>
           <div className="flex justify-around">
-            <p className="text-gray-50 text-[15px]">
+            <p className="text-gray-50 text-[12px] p-0 md:text-[15px]">
               {release_date || first_air_date}
             </p>
             <div className="w-[5px] h-[5px] bg-gray-100 rounded-md mt-2 mx-1"></div>
-            <p className="text-[15px] truncate">{genre_ids}</p>
+            <p className="text-[12px] md:text-[15px] p-0 truncate">
+              {genre_ids}
+            </p>
             <div>
               <p className="text-gray-50 text-[13px] border px-1 rounded-md ml-2">
                 {filmType === "tv_shows" ? "TvShow" : "Movie"}
